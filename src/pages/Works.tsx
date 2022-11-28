2f15205a28f4acba47f3c047177b301e9024972e
McWayne Otsuki <meadowmc17@gmail.com>
2022-11-28 15:32:46 +0800

    Update

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -41,26 +41,44 @@ export default function Works() {
             <Box sx={{ alignItems: 'center', justifyContent: 'center' }}>
 
                 <Grid rowSpacing={2} columnSpacing={{ xs: 5, sm: 5, md: 5 }} container >
+
                     <Grid item container >
                         <Card sx={{ maxWidth: 345, m: 5 }}>
                             <CardMedia
                                 component="img"
-                                height="140"
+                                width="140"
                                 image="/images/medsight.png"
                             />
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">MedSight</Typography>
                                 <Typography variant="body2" color="text.secondary" align="justify">
-                                    A Python Django application deployed using Microsoft Azure and Postgres database
+                                    A Doctor-Patient appointment system using Python Django application deployed using Microsoft Azure and Postgres database
                                 </Typography>
                             </CardContent>
                             <CardActions>
-                                <Link className="Links" to="/colorsequence">
+                                <a className="Links" href="https://github.com/ivananoosGithub/AppDevProject-MedSight" target="_blank">
                                     <Button size="small" sx={{ marginTop: -1, p: 2 }}>View</Button>
-                                </Link>
+                                </a>
                             </CardActions>
                         </Card>
-
+                        <Card sx={{ maxWidth: 345, m: 5 }}>
+                            <CardMedia
+                                component="img"
+                                width="140"
+                                image="/images/vorbestellan.png"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">Vorbestellan</Typography>
+                                <Typography variant="body2" color="text.secondary" align="justify">
+                                    A Conference Room reservation system Python Django application deployed using Microsoft Azure and Postgres database
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <a className="Links" href="https://github.com/McwayneOtsuki17/Vorbestellan" target="_blank">
+                                    <Button size="small" sx={{ marginTop: -1, p: 2 }}>View</Button>
+                                </a>
+                            </CardActions>
+                        </Card>
                     </Grid>
                 </Grid>
             </Box>
